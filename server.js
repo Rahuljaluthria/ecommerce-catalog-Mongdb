2b7e12bb09b366ee01adb5d904a8d3309c08ec9f
Rahuljaluthria <rahuljaluthria321@gmail.com>
2026-03-30 23:43:04 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 app.use('/api/products', require('./routes/productRoutes'));
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
